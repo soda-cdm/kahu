20b6eb1e7625f6d4dc116aa0ed5d198131883b05
Pravin Ranjan <PravinRanjan10@gmail.com>
2022-05-09 16:04:36 +0530

    Revert "controller code framework:v1i"
    This reverts commit 237001a72c85c0b9059b23a78e86c1d49f985018.

--- controllers/constants.go
+++
@@ -1,5 +0,0 @@
-package controllers
-
-const (
-	Backup = "backup"
-)
--- examples/backup_example.go
+++
@@ -1,7 +0,0 @@
-apiVersion: kahu.io/v1beta1
-kind: Backup
-metadata:
-   name: backup-demo6
-spec:
-   includedNamespaces: ["pod5"]
-   excludedNamespaces: ["pod7"]
