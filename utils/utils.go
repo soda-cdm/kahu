f8ae73831b0ca505a3cb1d4ba68095a4f50ba667
Pravin Ranjan <PravinRanjan10@gmail.com>
2022-06-22 15:09:19 +0530

    Updating utils

--- utils/utils.go
+++ utils/utils.go
@@ -15,17 +15,9 @@
 package utils
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"os"
-	"path/filepath"
 
-	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -38,64 +30,9 @@ func GetConfig(kubeConfig string) (config *restclient.Config, err error) {
 	return restclient.InClusterConfig()
 }
 
-func EnableLogTimeStamp() {
-	customFormatter := new(log.TextFormatter)
-	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
-	log.SetFormatter(customFormatter)
-	customFormatter.FullTimestamp = true
-}
-
 func NamespaceAndName(objMeta metav1.Object) string {
 	if objMeta.GetNamespace() == "" {
 		return objMeta.GetName()
 	}
 	return fmt.Sprintf("%s/%s", objMeta.GetNamespace(), objMeta.GetName())
-}
-
-func EnsureNamespaceExistsAndIsReady(namespace string, client corev1client.NamespaceInterface) (bool, error) {
-	// nsCreated tells whether the namespace was created by this method
-	// required for keeping track of number of restored items
-
-	_, err := client.Get(context.TODO(), namespace, metav1.GetOptions{})
-
-	if apierrors.IsNotFound(err) {
-		// Namespace isn't in cluster, we're good to create.
-		return true, nil
-	}
-	return true, nil
-}
-
-func configFileName() string {
-	return filepath.Join(os.Getenv("HOME"), ".config", "kahu", "config.json")
-}
-
-type kahuConfig map[string]interface{}
-
-// LoadConfig loads the Velero client configuration file and returns it as a VeleroConfig. If the
-// file does not exist, an empty map is returned.
-func LoadConfig() (kahuConfig, error) {
-	fileName := configFileName()
-
-	_, err := os.Stat(fileName)
-	if os.IsNotExist(err) {
-		// If the file isn't there, just return an empty map
-		return kahuConfig{}, nil
-	}
-	if err != nil {
-		// For any other Stat() error, return it
-		return nil, errors.WithStack(err)
-	}
-
-	configFile, err := os.Open(fileName)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	defer configFile.Close()
-
-	var config kahuConfig
-	if err := json.NewDecoder(configFile).Decode(&config); err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	return config, nil
 }
